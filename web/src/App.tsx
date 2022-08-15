f8bfe7776591e8051296c1a216578e209b077c56
Leonard Göhrs <leonard@goehrs.eu>
2022-08-15 15:21:10 +0200

    web: implement API picker
    The API picker is a button you can press that will switch all dynamic elements
    into an alternate mode which will direct you to the API definition for the
    respective element.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,39 +7,43 @@ import { Outlet } from "react-router-dom";
 import "@cloudscape-design/global-styles/index.css";
 
 import "./App.css";
+import { ApiPickerButton } from "./MqttComponents";
 
 function Navigation() {
   return (
-    <SideNavigation
-      header={{
-        href: "#",
-        logo: {
-          alt: "LXA TAC",
-          src: "/logo.svg",
-        },
-      }}
-      items={[
-        {
-          type: "section",
-          text: "Dashboards",
-          items: [
-            {
-              type: "link",
-              text: "Device Under Test",
-              href: "#/dashboard/dut",
-            },
-            { type: "link", text: "LXA TAC System", href: "#/dashboard/tac" },
-          ],
-        },
-        {
-          type: "section",
-          text: "Settings",
-          items: [
-            { type: "link", text: "Labgrid", href: "#/settings/labgrid" },
-          ],
-        },
-      ]}
-    />
+    <>
+      <SideNavigation
+        header={{
+          href: "#",
+          logo: {
+            alt: "LXA TAC",
+            src: "/logo.svg",
+          },
+        }}
+        items={[
+          {
+            type: "section",
+            text: "Dashboards",
+            items: [
+              {
+                type: "link",
+                text: "Device Under Test",
+                href: "#/dashboard/dut",
+              },
+              { type: "link", text: "LXA TAC System", href: "#/dashboard/tac" },
+            ],
+          },
+          {
+            type: "section",
+            text: "Settings",
+            items: [
+              { type: "link", text: "Labgrid", href: "#/settings/labgrid" },
+            ],
+          },
+        ]}
+      />
+      <ApiPickerButton />
+    </>
   );
 }
 
--- web/src/MqttComponents.tsx
+++ web/src/MqttComponents.tsx
@@ -12,6 +12,70 @@ import { MixedLineBarChartProps } from "@cloudscape-design/components/mixed-line
 
 import { useMqttSubscription, useMqttState } from "./mqtt";
 
+var api_pickers = new Set<(state: boolean) => void>();
+
+export function ApiPickerButton() {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    api_pickers.add(setActive);
+
+    return () => {
+      api_pickers.delete(setActive);
+    };
+  }, []);
+
+  return (
+    <Button
+      onClick={(ev) => api_pickers.forEach((v) => v(!active))}
+      formAction="none"
+      iconName="search"
+    >
+      API docs
+    </Button>
+  );
+}
+
+interface ApiPickerProps {
+  topic: string;
+  children: React.ReactNode;
+}
+
+export function ApiPicker(props: ApiPickerProps) {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    api_pickers.add(setActive);
+
+    return () => {
+      api_pickers.delete(setActive);
+    };
+  }, []);
+
+  function click(ev: React.MouseEvent<HTMLElement>) {
+    if (active) {
+      let tab = window.open(`/v1/api_doc/#${props.topic}`, "_blank");
+      if (tab !== null) {
+        tab.focus();
+      }
+
+      api_pickers.forEach((v) => v(false));
+
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
+  }
+
+  let outer_class = active ? "api_picker_outer_active" : "api_picker_outer";
+  let inner_class = active ? "api_picker_inner_active" : "api_picker_inner";
+
+  return (
+    <div className={outer_class} onClick={click}>
+      <div className={inner_class}>{props.children}</div>
+    </div>
+  );
+}
+
 interface MqttBoxProps<T> {
   topic: string;
   variant?: BoxProps.Variant;
@@ -23,14 +87,16 @@ export function MqttBox<T>(props: MqttBoxProps<T>) {
   const payload = useMqttSubscription<T>(props.topic, props.initial);
 
   if (payload === undefined) {
-    return (
+    var box = (
       <Box variant={props.variant}>
         <Spinner />
       </Box>
     );
   } else {
-    return <Box variant={props.variant}>{props.format(payload)}</Box>;
+    var box = <Box variant={props.variant}>{props.format(payload)}</Box>;
   }
+
+  return ApiPicker({ topic: props.topic, children: box });
 }
 
 interface MqttToggleConvProps<T> {
@@ -44,13 +110,15 @@ export function MqttToggleConv<T>(props: MqttToggleConvProps<T>) {
   const [settled, payload, setPayload] = useMqttState<T>(props.topic);
 
   return (
-    <Toggle
-      onChange={(ev) => setPayload(props.from_bool(ev.detail.checked))}
-      checked={payload === undefined ? false : props.to_bool(payload)}
-      disabled={!settled}
-    >
-      {props.children}
-    </Toggle>
+    <ApiPicker topic={props.topic}>
+      <Toggle
+        onChange={(ev) => setPayload(props.from_bool(ev.detail.checked))}
+        checked={payload === undefined ? false : props.to_bool(payload)}
+        disabled={!settled}
+      >
+        {props.children}
+      </Toggle>
+    </ApiPicker>
   );
 }
 
@@ -79,13 +147,15 @@ export function MqttButton<T>(props: MqttButtonProps<T>) {
   const [_settled, _payload, setPayload] = useMqttState<T>(props.topic);
 
   return (
-    <Button
-      formAction="none"
-      iconName={props.iconName}
-      onClick={() => setPayload(props.send)}
-    >
-      {props.children}
-    </Button>
+    <ApiPicker topic={props.topic}>
+      <Button
+        formAction="none"
+        iconName={props.iconName}
+        onClick={() => setPayload(props.send)}
+      >
+        {props.children}
+      </Button>
+    </ApiPicker>
   );
 }
 
@@ -121,13 +191,15 @@ export function MqttBarMeter<T>(props: MqttBarMeterProps<T>) {
   }
 
   return (
-    <ProgressBar
-      status={valid ? "in-progress" : "error"}
-      value={value}
-      description={description}
-      additionalInfo={props.additionalInfo}
-      label={label}
-    />
+    <ApiPicker topic={props.topic}>
+      <ProgressBar
+        status={valid ? "in-progress" : "error"}
+        value={value}
+        description={description}
+        additionalInfo={props.additionalInfo}
+        label={label}
+      />
+    </ApiPicker>
   );
 }
 
@@ -186,17 +258,19 @@ export function MqttChart(props: MqttChartProps) {
   };
 
   return (
-    <LineChart
-      series={[series]}
-      visibleSeries={[series]}
-      xScaleType="time"
-      i18nStrings={{
-        xTickFormatter: (e) =>
-          ((Number(e) - Number(end)) / 1000).toFixed(1) + "s",
-      }}
-      height={200}
-      hideFilter
-      hideLegend
-    />
+    <ApiPicker topic={props.topic}>
+      <LineChart
+        series={[series]}
+        visibleSeries={[series]}
+        xScaleType="time"
+        i18nStrings={{
+          xTickFormatter: (e) =>
+            ((Number(e) - Number(end)) / 1000).toFixed(1) + "s",
+        }}
+        height={200}
+        hideFilter
+        hideLegend
+      />
+    </ApiPicker>
   );
 }
