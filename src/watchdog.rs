0b6774caca865a79a9c88a159dda403af5466db1
Leonard Göhrs <leonard@goehrs.eu>
2022-08-19 11:47:39 +0200

    watchdog: use systemd-provided interval (/2) for feeding

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -2,12 +2,11 @@ use std::io::{Error, ErrorKind, Result};
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::Duration;
 
+use async_std::future::pending;
 use async_std::sync::Arc;
 use async_std::task::sleep;
 
-use systemd::daemon::{notify, STATE_READY, STATE_WATCHDOG};
-
-const WATCHDOG_INTERVAL: Duration = Duration::from_secs(1);
+use systemd::daemon::{notify, watchdog_enabled, STATE_READY, STATE_WATCHDOG};
 
 pub struct Watchdog {
     dut_power_tick: Arc<AtomicU32>,
@@ -27,16 +26,31 @@ impl Watchdog {
     /// - adc thread - if the adc values are too old dut_pwr_thread will
     ///   not increment the tick.
     pub async fn keep_fed(self) -> Result<()> {
+        let interval = {
+            let micros = watchdog_enabled(false).unwrap_or(0);
+
+            if micros == 0 {
+                eprintln!("Watchdog not requested. Disabling");
+
+                // Wait forever, as returning from this function terminated the program
+                let () = pending().await;
+            }
+
+            Duration::from_micros(micros) / 2
+        };
+
         notify(false, [(STATE_READY, "1")].iter())?;
 
         let mut prev = self.dut_power_tick.load(Ordering::Relaxed);
 
         loop {
-            sleep(WATCHDOG_INTERVAL).await;
+            sleep(interval).await;
 
             let curr = self.dut_power_tick.load(Ordering::Relaxed);
 
             if prev == curr {
+                eprintln!("Power Thread has stalled. Will trigger watchdog.");
+
                 notify(false, [(STATE_WATCHDOG, "trigger")].iter())?;
 
                 break Err(Error::new(
