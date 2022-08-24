e7ec6a9c3b15f02b14bb0c7f74d965795cb704a5
Leonard Göhrs <leonard@goehrs.eu>
2022-08-24 16:02:43 +0200

    mqtt: take the good parts from tide-websockets and remove the dependency
    The handshaking part of tide-websockets is quite useful, but the remaining
    integration does more harm than good:
    
    - The fact that your websocket handler should return a Result gives you the
      impression that there is some error handling or that the client is notified
      over what went wrong. This is however not the case.
    - The WebSocketConnection wrapper around the tungstenite stream only gives you
      a minimal set of operations. flushin and closing the stream is not among
      them.

--- src/broker/mqtt_conn.rs
+++ src/broker/mqtt_conn.rs
@@ -2,17 +2,26 @@ use std::collections::HashMap;
 use std::io::Cursor;
 
 use async_std::channel::bounded;
-use async_std::prelude::*;
-use async_std::sync::Arc;
+use async_std::sync::{Arc, Mutex};
 use async_std::task::spawn;
 
-use tide_websockets::{WebSocket, WebSocketConnection};
+use async_tungstenite::tungstenite::{protocol::Role, Message};
+use async_tungstenite::WebSocketStream;
+
+use futures_util::{SinkExt, StreamExt};
 
 use mqtt::control::variable_header::{ConnectReturnCode, ProtocolLevel};
 use mqtt::packet::publish::QoSWithPacketIdentifier;
 use mqtt::packet::suback::SubscribeReturnCode;
 use mqtt::TopicFilter;
 use mqtt::{packet::*, Decodable, Encodable};
+
+use sha1::{Digest, Sha1};
+
+use tide::http::format_err;
+use tide::http::headers::{HeaderName, CONNECTION, UPGRADE};
+use tide::http::upgrade::Connection;
+use tide::{Request, Response, StatusCode};
 
 pub use mqtt::TopicName;
 
@@ -25,16 +34,23 @@ use super::{AnySubscriptionHandle, AnyTopic};
 /// gets a visual indication that the web interface is no longer up to date.
 const MAX_QUEUE_LENGTH: usize = 256;
 
+/// Force a flush on the Websocket every now and then to make sure that
+/// the backpressure mechanism mentioned above actually does something.
+const MAX_PENDING_BYTES: usize = 256 * 1024;
+
+/// This is used in the WebSocket handshake
+const WEBSOCKET_GUID: &str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
 // The mqtt crate provides the Decodable and Encodable traits that can decode/
 // encode packets from/to Readers/Writers.
-// This is nice, but we use Vec<u8> instead of Readers/Writers.
-// Provide convenience wrappers that use/provide Vec<u8> directly.
+// This is nice, but we use WebSocket Messages instead of Readers/Writers.
+// Provide convenience wrappers that use/provide Messages directly.
 trait DecodableExt: Decodable
 where
     <Self as Decodable>::Cond: Default,
 {
-    fn from_bytes(bytes: Vec<u8>) -> Result<Self, Self::Error> {
-        Self::decode(&mut Cursor::new(bytes))
+    fn from_message(msg: Message) -> Result<Self, Self::Error> {
+        Self::decode(&mut Cursor::new(msg.into_data()))
     }
 }
 
@@ -46,10 +62,10 @@ where
 }
 
 trait EncodableExt: Encodable {
-    fn as_bytes(&self) -> std::io::Result<Vec<u8>> {
+    fn as_message(&self) -> std::io::Result<Message> {
         let mut cursor = Cursor::new(Vec::new());
         self.encode(&mut cursor)?;
-        Ok(cursor.into_inner())
+        Ok(Message::binary(cursor.into_inner()))
     }
 }
 
@@ -59,8 +75,8 @@ impl<E> EncodableExt for E where E: Encodable {}
 /// from protocol handshake to teardown.
 async fn handle_connection(
     topics: Arc<Vec<Arc<dyn AnyTopic>>>,
-    mut stream: WebSocketConnection,
-) -> tide::Result<()> {
+    mut stream: WebSocketStream<Connection>,
+) {
     // The MQTT connection starts with a CONNECT packet.
     // Since we are only targeting the one MQTT (over WebSockets)
     // implementation used in the web interface we can make some assumptions.
@@ -68,19 +84,15 @@ async fn handle_connection(
     // Websocket frames. If we would want to support MQTT over raw TCP as well
     // we would have to use the length fields contained in the MQTT packets.
     let conn_pkg = {
-        let msg = stream
-            .next()
-            .await
-            .ok_or_else(|| tide::Error::from_str(500, "Unexpected end of stream"))??
-            .into_data();
+        let msg = match stream.next().await {
+            Some(Ok(msg)) => msg,
+            _ => return,
+        };
 
-        match VariablePacket::from_bytes(msg)? {
-            VariablePacket::ConnectPacket(conn) => Ok(conn),
-            _ => Err(tide::Error::from_str(
-                500,
-                "Protocol violation. Expected CONNECT",
-            )),
-        }?
+        match VariablePacket::from_message(msg) {
+            Ok(VariablePacket::ConnectPacket(conn)) => conn,
+            _ => return,
+        }
     };
 
     // The second assumption is that the client will always use the same MQTT
@@ -92,34 +104,64 @@ async fn handle_connection(
         || conn_pkg.will_retain()
         || conn_pkg.protocol_level() != ProtocolLevel::Version311
     {
-        Err(tide::Error::from_str(
-            500,
-            "Client does not implement the expected MQTT subset",
-        ))?
+        return;
     }
 
     // Send CONNACK packet to signal a successful connection setup
-    stream
-        .send_bytes(ConnackPacket::new(false, ConnectReturnCode::ConnectionAccepted).as_bytes()?)
-        .await?;
+    if stream
+        .send(
+            ConnackPacket::new(false, ConnectReturnCode::ConnectionAccepted)
+                .as_message()
+                .unwrap(),
+        )
+        .await
+        .is_err()
+    {
+        return;
+    }
+
+    if stream.flush().await.is_err() {
+        return;
+    }
+
+    let (stream_tx, mut stream_rx) = stream.split();
+    let stream_tx = Arc::new(Mutex::new(stream_tx));
 
     // Set up a task that takes messages from a queue, wraps them in a MQTT
     // packet and sends them out over the websocket.
     // This should generate backpressure on the queue if the websocket can not
     // make progress and the senders should close the queue if it is full.
-    // FIXME: the queue being closed will only result in this task ending but
-    // not in the WebSocket being closed. This needs to be fixed but
-    // tide_websockets does not provide us with a way to explicitly close the socket.
     let (to_websocket, mut for_websocket) = bounded::<(TopicName, Arc<[u8]>)>(MAX_QUEUE_LENGTH);
-    let stream_tx = stream.clone();
+    let stream_tx_task = stream_tx.clone();
     spawn(async move {
+        let mut pending_bytes = 0;
+
         while let Some((topic, payload)) = for_websocket.next().await {
             let pkg = PublishPacket::new(topic, QoSWithPacketIdentifier::Level0, payload.to_vec());
 
-            if let Err(_) = stream_tx.send_bytes(pkg.as_bytes().unwrap()).await {
+            let msg = match pkg.as_message() {
+                Ok(msg) => msg,
+                Err(_) => break,
+            };
+
+            pending_bytes += msg.len();
+
+            // Enqueue the message for sending
+            if stream_tx_task.lock().await.send(msg).await.is_err() {
                 break;
             }
+
+            // Make sure that every now and then the messages are actually sent out
+            if pending_bytes > MAX_PENDING_BYTES {
+                if stream_tx_task.lock().await.flush().await.is_err() {
+                    break;
+                }
+                pending_bytes = 0;
+            }
         }
+
+        // Something went wrong. Make sure that the client is informed about it.
+        stream_tx_task.lock().await.close().await
     });
 
     // Keep track of the currently subscribed topics to be able to handle
@@ -128,13 +170,13 @@ async fn handle_connection(
         HashMap::new();
 
     // Handle packets sent by the client
-    'connection: while let Some(pkg) = stream
+    'connection: while let Some(pkg) = stream_rx
         .next()
         .await
         .transpose()
         .ok()
         .flatten()
-        .map(|msg| VariablePacket::from_bytes(msg.into_data()).ok())
+        .map(|msg| VariablePacket::from_message(msg).ok())
         .flatten()
     {
         match pkg {
@@ -147,13 +189,13 @@ async fn handle_connection(
                         .map(|_| SubscribeReturnCode::MaximumQoSLevel0)
                         .collect(),
                 )
-                .as_bytes()
+                .as_message()
                 .unwrap();
 
                 // We should get the suback out before sending the retained
                 // values. So send it now even though we did not do the
                 // subscribing yet.
-                if stream.send_bytes(suback_pkg).await.is_err() {
+                if stream_tx.lock().await.send(suback_pkg).await.is_err() {
                     break 'connection;
                 }
 
@@ -213,10 +255,10 @@ async fn handle_connection(
                 }
 
                 let unsuback_pkg = UnsubackPacket::new(unsub_pkg.packet_identifier())
-                    .as_bytes()
+                    .as_message()
                     .unwrap();
 
-                if stream.send_bytes(unsuback_pkg).await.is_err() {
+                if stream_tx.lock().await.send(unsuback_pkg).await.is_err() {
                     break 'connection;
                 }
             }
@@ -240,9 +282,9 @@ async fn handle_connection(
                 }
             }
             VariablePacket::PingreqPacket(_) => {
-                let pingresp_pkg = PingrespPacket::new().as_bytes().unwrap();
+                let pingresp_pkg = PingrespPacket::new().as_message().unwrap();
 
-                if stream.send_bytes(pingresp_pkg).await.is_err() {
+                if stream_tx.lock().await.send(pingresp_pkg).await.is_err() {
                     break 'connection;
                 }
             }
@@ -253,13 +295,70 @@ async fn handle_connection(
     for desub in subscription_handles.into_values().flatten() {
         desub.unsubscribe().await
     }
+}
 
-    Ok(())
+fn header_contains_ignore_case(req: &Request<()>, header_name: HeaderName, value: &str) -> bool {
+    req.header(header_name)
+        .map(|h| {
+            h.as_str()
+                .split(',')
+                .any(|s| s.trim().eq_ignore_ascii_case(value.trim()))
+        })
+        .unwrap_or(false)
 }
 
 pub(super) fn register(server: &mut tide::Server<()>, topics: Arc<Vec<Arc<dyn AnyTopic>>>) {
-    server.at("/v1/mqtt").get(
-        WebSocket::new(move |_request, stream| handle_connection(topics.clone(), stream))
-            .with_protocols(&["mqttv3.1", "mqtt"]),
-    );
+    server.at("/v1/mqtt").get(move |req: Request<()>| {
+        let topics = topics.clone();
+
+        async move {
+            // These are the good parts from tide-websockets without the bad
+            // WebSocketConnection wrapper.
+
+            let connection_upgrade = header_contains_ignore_case(&req, CONNECTION, "upgrade");
+            let upgrade_to_websocket = header_contains_ignore_case(&req, UPGRADE, "websocket");
+            let upgrade_requested = connection_upgrade && upgrade_to_websocket;
+
+            if !upgrade_requested {
+                return Ok(Response::new(StatusCode::UpgradeRequired));
+            }
+
+            let header = match req.header("Sec-Websocket-Key") {
+                Some(h) => h.as_str(),
+                None => return Err(format_err!("expected sec-websocket-key")),
+            };
+
+            let protocol = req.header("Sec-Websocket-Protocol").and_then(|value| {
+                value
+                    .as_str()
+                    .split(',')
+                    .map(str::trim)
+                    .find(|req_p| req_p == &"mqttv3.1" || req_p == &"mqtt")
+            });
+
+            let mut response = Response::new(StatusCode::SwitchingProtocols);
+
+            response.insert_header(UPGRADE, "websocket");
+            response.insert_header(CONNECTION, "Upgrade");
+            let hash = Sha1::new().chain(header).chain(WEBSOCKET_GUID).finalize();
+            response.insert_header("Sec-Websocket-Accept", base64::encode(&hash[..]));
+            response.insert_header("Sec-Websocket-Version", "13");
+
+            if let Some(protocol) = protocol {
+                response.insert_header("Sec-Websocket-Protocol", protocol);
+            }
+
+            let http_res: &mut tide::http::Response = response.as_mut();
+            let upgrade_receiver = http_res.recv_upgrade().await;
+
+            spawn(async move {
+                if let Some(stream) = upgrade_receiver.await {
+                    let ws = WebSocketStream::from_raw_socket(stream, Role::Server, None).await;
+                    handle_connection(topics, ws).await;
+                }
+            });
+
+            Ok(response)
+        }
+    });
 }
