20b2675198ed123ce40d8ef46e98c0f5c2bb3ef3
Leonard Göhrs <l.goehrs@pengutronix.de>
2022-12-12 08:44:17 +0100

    tacd: web: serve static files from disk
    This is makes the standalone tacd usable with an externally built web
    interface, but is not yet as good as the previous solutions using the compiled
    in webinterface or a fully grown nginx to serve the interface.
    
    What is lacking:
    
      - Cache Header handling: If-Modified-Since or ETags
      - Compression: Serve pre-compressed files and decompress on the fly?
        Serve uncompressed files and compress on the fly?
        Cache (small) compressed files?
      - Directory listings for user files
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/web.rs
+++ src/web.rs
@@ -15,9 +15,13 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+use std::convert::AsRef;
 use std::fs::write;
+use std::io::ErrorKind;
 use std::net::TcpListener;
+use std::path::Path;
 
+use log::warn;
 use tide::{Request, Response, Server};
 
 #[cfg(any(test, feature = "stub_out_root"))]
@@ -25,6 +29,8 @@ mod sd {
     use std::io::Result;
     use std::net::TcpListener;
 
+    pub const WEBUI_DIR: &str = "web/build";
+    pub const USER_DIR: &str = "srv/www";
     pub const FALLBACK_PORT: &str = "[::]:8080";
 
     pub fn listen_fds(_: bool) -> Result<[(); 0]> {
@@ -40,10 +46,12 @@ mod sd {
 mod sd {
     pub use systemd::daemon::*;
 
+    pub const WEBUI_DIR: &str = "/usr/share/tacd/webui";
+    pub const USER_DIR: &str = "/srv/www";
     pub const FALLBACK_PORT: &str = "[::]:80";
 }
 
-use sd::{listen_fds, tcp_listener, FALLBACK_PORT};
+use sd::{listen_fds, tcp_listener, FALLBACK_PORT, USER_DIR, WEBUI_DIR};
 
 pub struct WebInterface {
     listeners: Vec<TcpListener>,
@@ -73,11 +81,14 @@ impl WebInterface {
         }
 
         this.expose_openapi_json();
+        this.expose_dir(WEBUI_DIR, "/");
+        this.expose_dir(USER_DIR, "/srv/");
 
         this
     }
 
-    pub fn expose_openapi_json(&mut self) {
+    /// Serve a compiled-in openapi.json file
+    fn expose_openapi_json(&mut self) {
         self.server
             .at("/v1/openapi.json")
             .get(|req: Request<()>| async move {
@@ -88,7 +99,49 @@ impl WebInterface {
             });
     }
 
-    // Serve a file from disk for reading and writing
+    /// Serve a directory from disk for reading
+    fn expose_dir(&mut self, fs_path: impl AsRef<Path>, web_path: &str) {
+        if let Err(e) = self.server.at(web_path).serve_dir(&fs_path) {
+            // Don't crash if the directory does not exist.
+            // Just print a warning.
+            match e.kind() {
+                ErrorKind::NotFound => {
+                    warn!(
+                        "Can not serve {} at {}: Directory not found",
+                        fs_path.as_ref().display(),
+                        web_path
+                    );
+                }
+                _ => Err(e).unwrap(),
+            }
+        }
+
+        // Serve an index.html if the bare directory path is requested.
+        // This only works for the top level. If we want to serve index.htmls
+        // from sub-directories we would have to modify serve_dir().
+        // Which is something we will likely want anyways as it does not
+        // support compression, caching headers or directory listings.
+        if web_path.ends_with("/") {
+            let index_html = fs_path.as_ref().join("index.html");
+
+            if let Err(e) = self.server.at(&web_path).serve_file(&index_html) {
+                // Don't crash if the directory does not exist. Just print a
+                // warning.
+                match e.kind() {
+                    ErrorKind::NotFound => {
+                        warn!(
+                            "Can not serve {} at {}: File not found",
+                            index_html.display(),
+                            web_path
+                        );
+                    }
+                    _ => Err(e).unwrap(),
+                }
+            }
+        }
+    }
+
+    /// Serve a file from disk for reading and writing
     pub fn expose_file_rw(&mut self, fs_path: &str, web_path: &str) {
         self.server.at(web_path).serve_file(fs_path).unwrap();
 
