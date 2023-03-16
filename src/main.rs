b0c75e11109c7acff6ab5991e467e744dd3861bc
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-03-16 10:59:05 +0100

    main: re-order and document to provide a friendly entry point
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/main.rs
+++ src/main.rs
@@ -49,37 +49,74 @@ use web::WebInterface;
 async fn main() -> Result<(), std::io::Error> {
     pretty_env_logger::init();
 
+    // The BrokerBuilder collects topics that should be exported via the
+    // MQTT/REST APIs.
+    // The topics are also used to pass around data inside the tacd.
     let mut bb = BrokerBuilder::new();
 
+    // Expose hardware on the TAC via the broker framework.
     let adc = Adc::new(&mut bb).await.unwrap();
     let dut_pwr = DutPwrThread::new(&mut bb, adc.pwr_volt.clone(), adc.pwr_curr.clone())
         .await
         .unwrap();
-    let watchdog = Watchdog::new(dut_pwr.tick());
+    let dig_io = DigitalIo::new(&mut bb);
+    let temperatures = Temperatures::new(&mut bb);
+    let usb_hub = UsbHub::new(&mut bb);
 
-    let dbus = DbusSession::new(&mut bb).await;
+    // Expose other software on the TAC via the broker framework by connecting
+    // to them via HTTP / DBus APIs.
+    let iobus = IoBus::new(&mut bb);
+    let (network, rauc, systemd) = {
+        let dbus = DbusSession::new(&mut bb).await;
 
-    let resources = UiResources {
-        adc,
-        network: dbus.network,
-        rauc: dbus.rauc,
-        systemd: dbus.systemd,
-        dig_io: DigitalIo::new(&mut bb),
-        dut_pwr,
-        iobus: IoBus::new(&mut bb),
-        system: System::new(&mut bb),
-        temperatures: Temperatures::new(&mut bb),
-        usb_hub: UsbHub::new(&mut bb),
+        (dbus.network, dbus.rauc, dbus.systemd)
     };
 
+    // Expose information about the system provided by the kernel via the
+    // broker framework.
+    let system = System::new(&mut bb);
+
+    // Make sure the ADC and power switching threads of the tacd are not
+    // stalled for too long by providing watchdog events to systemd
+    // (if requested on start).
+    let watchdog = Watchdog::new(dut_pwr.tick());
+
+    // Set up a web server and provide some static files like the web
+    // interface and config files that may be edited inside the web ui.
     let mut web_interface = WebInterface::new();
 
+    // Expose a live log of the TAC's systemd journal so it can be viewed
+    // in the web interface.
     journal::serve(&mut web_interface.server);
-    let ui = Ui::new(&mut bb, resources, &mut web_interface.server);
+
+    // Set up the user interface for the hardware display on the TAC.
+    // The different screens receive updates via the topics provided in
+    // the UiResources struct.
+    let ui = {
+        let resources = UiResources {
+            adc,
+            network,
+            rauc,
+            systemd,
+            dig_io,
+            dut_pwr,
+            iobus,
+            system,
+            temperatures,
+            usb_hub,
+        };
+
+        Ui::new(&mut bb, resources, &mut web_interface.server)
+    };
+
+    // Consume the BrokerBuilder (no further topics can be added or removed)
+    // and expose the topics via HTTP and MQTT-over-websocket.
     bb.build(&mut web_interface.server);
 
     log::info!("Setup complete. Handling requests");
 
+    // Run until the web server, user interface or (if selected) the watchdog
+    // exits (with an error).
     if let Some(watchdog) = watchdog {
         select! {
             ui_err = ui.run().fuse() => ui_err,
