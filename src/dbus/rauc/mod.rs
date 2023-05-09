d9a65f2ab58d17937415153bdbfdb71b3dd7099b
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-05-09 09:40:18 +0200

    dbus: rauc: update_channels: add a concept of update channels
    On vanilla TACs we are going to have two official update channels
    for operating system bundles, Stable and Testing.
    
    In addition to that users may also choose to add their own custom
    channels.
    
    Add concept of these channels and a way to poll them to
    check if a new update is available.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/dbus/rauc/mod.rs
+++ src/dbus/rauc/mod.rs
@@ -15,25 +15,66 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
+use async_std::channel::Receiver;
+use async_std::stream::StreamExt;
 use async_std::sync::Arc;
+use async_std::task::{sleep, spawn, JoinHandle};
+use log::warn;
 use serde::{Deserialize, Serialize};
-
-#[cfg(not(feature = "demo_mode"))]
-use async_std::prelude::*;
-
-#[cfg(not(feature = "demo_mode"))]
-use async_std::task::spawn;
 
 use super::Connection;
 use crate::broker::{BrokerBuilder, Topic};
+
+mod update_channels;
+pub use update_channels::Channel;
 
 #[cfg(feature = "demo_mode")]
 mod demo_mode;
 
 #[cfg(not(feature = "demo_mode"))]
 mod installer;
+
+#[cfg(not(feature = "demo_mode"))]
+use installer::InstallerProxy;
+
+#[cfg(feature = "demo_mode")]
+mod imports {
+    pub struct InstallerProxy<'a> {
+        _dummy: &'a (),
+    }
+
+    impl<'a> InstallerProxy<'a> {
+        pub async fn new<C>(_conn: C) -> Option<InstallerProxy<'a>> {
+            Some(Self { _dummy: &() })
+        }
+
+        pub async fn info(&self, _url: &str) -> anyhow::Result<(String, String)> {
+            let compatible = "LXA TAC".to_string();
+            let version = "4.0-0-20230428214619".to_string();
+
+            Ok((compatible, version))
+        }
+    }
+
+    pub const CHANNELS_DIR: &str = "demo_files/usr/share/tacd/update_channels";
+}
+
+#[cfg(not(feature = "demo_mode"))]
+mod imports {
+    pub use anyhow::{anyhow, bail, Result};
+    pub use futures::{select, FutureExt};
+    pub use log::{error, info};
+
+    pub const CHANNELS_DIR: &str = "/usr/share/tacd/update_channels";
+}
+
+const RELOAD_RATE_LIMIT: Duration = Duration::from_secs(10 * 60);
+
+use imports::*;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Progress {
@@ -60,6 +101,163 @@ pub struct Rauc {
     pub slot_status: Arc<Topic<Arc<SlotStatus>>>,
     pub last_error: Arc<Topic<String>>,
     pub install: Arc<Topic<String>>,
+    pub channels: Arc<Topic<Vec<Channel>>>,
+    pub reload: Arc<Topic<bool>>,
+    pub should_reboot: Arc<Topic<bool>>,
+}
+
+fn compare_versions(v1: &str, v2: &str) -> Option<Ordering> {
+    // Version strings look something like this: "4.0-0-20230428214619"
+    // Use string sorting on the date part to determine which bundle is newer.
+    let date_1 = v1.rsplit_once('-').map(|(_, d)| d);
+    let date_2 = v2.rsplit_once('-').map(|(_, d)| d);
+
+    // Return Sone if either version could not be split or a Some with the
+    // ordering between the dates.
+    date_1.zip(date_2).map(|(d1, d2)| d1.cmp(d2))
+}
+
+#[cfg(not(feature = "demo_mode"))]
+fn booted_older_than_other(slot_status: &SlotStatus) -> Result<bool> {
+    let rootfs_0 = slot_status.get("rootfs_0");
+    let rootfs_1 = slot_status.get("rootfs_1");
+
+    let rootfs_0_booted = rootfs_0.and_then(|r| r.get("state")).map(|s| s == "booted");
+    let rootfs_1_booted = rootfs_1.and_then(|r| r.get("state")).map(|s| s == "booted");
+
+    let (booted, other) = match (rootfs_0_booted, rootfs_1_booted) {
+        (Some(true), Some(true)) => {
+            bail!("Two booted RAUC slots at the same time");
+        }
+        (Some(true), _) => (rootfs_0, rootfs_1),
+        (_, Some(true)) => (rootfs_1, rootfs_0),
+        _ => {
+            bail!("No booted RAUC slot");
+        }
+    };
+
+    // Not having version information for the booted slot is an error.
+    let booted_version = booted
+        .and_then(|r| r.get("bundle_version"))
+        .ok_or(anyhow!("No bundle version information for booted slot"))?;
+
+    // Not having version information for the other slot just means that
+    // it is not newer.
+    if let Some(other_version) = other.and_then(|r| r.get("bundle_version")) {
+        if let Some(rel) = compare_versions(other_version, booted_version) {
+            Ok(rel.is_gt())
+        } else {
+            Err(anyhow!(
+                "Failed to compare date for bundle versions \"{}\" and \"{}\"",
+                other_version,
+                booted_version
+            ))
+        }
+    } else {
+        Ok(false)
+    }
+}
+
+async fn channel_polling_task(
+    conn: Arc<Connection>,
+    channels: Arc<Topic<Vec<Channel>>>,
+    slot_status: Arc<Topic<Arc<SlotStatus>>>,
+    name: String,
+) {
+    let proxy = InstallerProxy::new(&conn).await.unwrap();
+
+    while let Some(mut channel) = channels
+        .try_get()
+        .and_then(|chs| chs.into_iter().find(|ch| ch.name == name))
+    {
+        let polling_interval = channel.polling_interval;
+        let slot_status = slot_status.try_get();
+
+        if let Err(e) = channel.poll(&proxy, slot_status.as_deref()).await {
+            warn!(
+                "Failed to fetch update for update channel \"{}\": {}",
+                channel.name, e
+            );
+        }
+
+        channels.modify(|chs| {
+            let mut chs = chs?;
+            let channel_prev = chs.iter_mut().find(|ch| ch.name == name)?;
+
+            // Check if the bundle we polled is the same as before and we don't need
+            // to send a message to the subscribers.
+            if *channel_prev == channel {
+                return None;
+            }
+
+            // Update the channel description with the newly polled bundle info
+            *channel_prev = channel;
+
+            Some(chs)
+        });
+
+        match polling_interval {
+            Some(pi) => sleep(pi).await,
+            None => break,
+        }
+    }
+}
+
+async fn channel_list_update_task(
+    conn: Arc<Connection>,
+    mut reload_stream: Receiver<bool>,
+    channels: Arc<Topic<Vec<Channel>>>,
+    slot_status: Arc<Topic<Arc<SlotStatus>>>,
+) {
+    let mut previous: Option<Instant> = None;
+    let mut polling_tasks: Vec<JoinHandle<_>> = Vec::new();
+
+    while let Some(reload) = reload_stream.next().await {
+        if !reload {
+            continue;
+        }
+
+        // Polling for updates is a somewhat expensive operation.
+        // Make sure it can not be abused to DOS the tacd.
+        if previous
+            .map(|p| p.elapsed() < RELOAD_RATE_LIMIT)
+            .unwrap_or(false)
+        {
+            continue;
+        }
+
+        // Read the list of available update channels
+        let new_channels = match Channel::from_directory(CHANNELS_DIR) {
+            Ok(chs) => chs,
+            Err(e) => {
+                warn!("Failed to get list of update channels: {e}");
+                continue;
+            }
+        };
+
+        // Stop the currently running polling tasks
+        for task in polling_tasks.drain(..) {
+            task.cancel().await;
+        }
+
+        let names: Vec<String> = new_channels.iter().map(|c| c.name.clone()).collect();
+
+        channels.set(new_channels);
+
+        // Spawn new polling tasks. They will poll once immediately.
+        for name in names.into_iter() {
+            let polling_task = spawn(channel_polling_task(
+                conn.clone(),
+                channels.clone(),
+                slot_status.clone(),
+                name,
+            ));
+
+            polling_tasks.push(polling_task);
+        }
+
+        previous = Some(Instant::now());
+    }
 }
 
 impl Rauc {
@@ -70,6 +268,9 @@ impl Rauc {
             slot_status: bb.topic_ro("/v1/tac/update/slots", None),
             last_error: bb.topic_ro("/v1/tac/update/last_error", None),
             install: bb.topic_wo("/v1/tac/update/install", Some("".to_string())),
+            channels: bb.topic_ro("/v1/tac/update/channels", None),
+            reload: bb.topic_wo("/v1/tac/update/channels/reload", Some(true)),
+            should_reboot: bb.topic_ro("/v1/tac/update/should_reboot", Some(false)),
         }
     }
 
@@ -81,6 +282,15 @@ impl Rauc {
         inst.slot_status.set(Arc::new(demo_mode::slot_status()));
         inst.last_error.set("".to_string());
 
+        // Reload the channel list on request
+        let (reload_stream, _) = inst.reload.clone().subscribe_unbounded();
+        spawn(channel_list_update_task(
+            Arc::new(Connection),
+            reload_stream,
+            inst.channels.clone(),
+            inst.slot_status.clone(),
+        ));
+
         inst
     }
 
@@ -91,9 +301,11 @@ impl Rauc {
         let conn_task = conn.clone();
         let operation = inst.operation.clone();
         let slot_status = inst.slot_status.clone();
+        let channels = inst.channels.clone();
+        let should_reboot = inst.should_reboot.clone();
 
         spawn(async move {
-            let proxy = installer::InstallerProxy::new(&conn_task).await.unwrap();
+            let proxy = InstallerProxy::new(&conn_task).await.unwrap();
 
             let mut stream = proxy.receive_operation_changed().await;
 
@@ -143,6 +355,30 @@ impl Rauc {
                         })
                         .collect();
 
+                    // Update the `newer_than_installed` field for the upstream bundles inside
+                    // of the update channels.
+                    channels.modify(|prev| {
+                        let prev = prev?;
+
+                        let mut new = prev.clone();
+
+                        for ch in new.iter_mut() {
+                            if let Some(bundle) = ch.bundle.as_mut() {
+                                bundle.update_install(&slots);
+                            }
+                        }
+
+                        // Only send out messages if anything changed
+                        (new != prev).then_some(new)
+                    });
+
+                    // Provide a simple yes/no "should reboot into other slot?" information
+                    // based on the bundle versions in the booted slot and the other slot.
+                    match booted_older_than_other(&slots) {
+                        Ok(b) => should_reboot.set_if_changed(b),
+                        Err(e) => warn!("Could not determine if TAC should be rebooted: {e}"),
+                    }
+
                     // In the RAUC API the slot status is a list of (name, info) tuples.
                     // It is once again easier in typescript to represent it as a dict with
                     // the names as keys, so that is what's exposed here.
@@ -165,7 +401,7 @@ impl Rauc {
 
         // Forward the "progress" property to the broker framework
         spawn(async move {
-            let proxy = installer::InstallerProxy::new(&conn_task).await.unwrap();
+            let proxy = InstallerProxy::new(&conn_task).await.unwrap();
 
             let mut stream = proxy.receive_progress_changed().await;
 
@@ -185,7 +421,7 @@ impl Rauc {
 
         // Forward the "last_error" property to the broker framework
         spawn(async move {
-            let proxy = installer::InstallerProxy::new(&conn_task).await.unwrap();
+            let proxy = InstallerProxy::new(&conn_task).await.unwrap();
 
             let mut stream = proxy.receive_last_error_changed().await;
 
@@ -205,17 +441,27 @@ impl Rauc {
 
         // Forward the "install" topic from the broker framework to RAUC
         spawn(async move {
-            let proxy = installer::InstallerProxy::new(&conn_task).await.unwrap();
+            let proxy = InstallerProxy::new(&conn_task).await.unwrap();
 
             while let Some(url) = install_stream.next().await {
                 // Poor-mans validation. It feels wrong to let someone point to any
                 // file on the TAC from the web interface.
                 if url.starts_with("http://") || url.starts_with("https://") {
-                    // TODO: some kind of error handling
-                    let _ = proxy.install(&url).await;
+                    if let Err(e) = proxy.install(&url).await {
+                        error!("Failed to install bundle: {}", e);
+                    }
                 }
             }
         });
+
+        // Reload the channel list on request
+        let (reload_stream, _) = inst.reload.clone().subscribe_unbounded();
+        spawn(channel_list_update_task(
+            conn.clone(),
+            reload_stream,
+            inst.channels.clone(),
+            inst.slot_status.clone(),
+        ));
 
         inst
     }
---
+++ src/dbus/rauc/update_channels.rs
@@ -0,0 +1,209 @@
+// This file is part of tacd, the LXA TAC system daemon
+// Copyright (C) 2023 Pengutronix e.K.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use std::fs::{read_dir, read_to_string, DirEntry};
+use std::os::unix::ffi::OsStrExt;
+use std::path::Path;
+use std::time::Duration;
+
+use anyhow::{anyhow, bail, Result};
+use serde::{Deserialize, Serialize};
+
+use super::{compare_versions, InstallerProxy, SlotStatus};
+
+#[cfg(feature = "demo_mode")]
+const ENABLE_DIR: &str = "demo_files/etc/rauc/certificates-enabled";
+
+#[cfg(not(feature = "demo_mode"))]
+const ENABLE_DIR: &str = "/etc/rauc/certificates-enabled";
+
+const ONE_MINUTE: Duration = Duration::from_secs(60);
+const ONE_HOUR: Duration = Duration::from_secs(60 * 60);
+const ONE_DAY: Duration = Duration::from_secs(24 * 60 * 60);
+
+#[derive(Serialize, Deserialize, Clone, PartialEq)]
+pub struct UpstreamBundle {
+    pub compatible: String,
+    pub version: String,
+    pub newer_than_installed: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq)]
+pub struct Channel {
+    pub name: String,
+    pub display_name: String,
+    pub description: String,
+    pub url: String,
+    pub polling_interval: Option<Duration>,
+    pub enabled: bool,
+    pub bundle: Option<UpstreamBundle>,
+}
+
+#[derive(Deserialize)]
+pub struct ChannelFile {
+    pub name: String,
+    pub display_name: String,
+    pub description: String,
+    pub url: String,
+    pub polling_interval: Option<String>,
+}
+
+impl Channel {
+    fn from_file(path: &Path) -> Result<Self> {
+        let file_name = || {
+            path.file_name()
+                .and_then(|f| f.to_str())
+                .unwrap_or("<no filename>")
+        };
+
+        let mut channel_file: ChannelFile = {
+            let content = read_to_string(path)?;
+            serde_yaml::from_str(&content)?
+        };
+
+        let polling_interval = match channel_file.polling_interval.take() {
+            Some(mut pi) => {
+                let multiplier = match pi.pop() {
+                    Some('m') => ONE_MINUTE,
+                    Some('h') => ONE_HOUR,
+                    Some('d') => ONE_DAY,
+                    _ => {
+                        bail!(
+                        "The polling_interval in \"{}\" does not have one of m, h or d as suffix",
+                        file_name()
+                    );
+                    }
+                };
+
+                let value: u32 = pi.parse().map_err(|e| {
+                    anyhow!(
+                        "Failed to parse polling_interval in \"{}\": {}",
+                        file_name(),
+                        e
+                    )
+                })?;
+
+                (value != 0).then_some(multiplier * value)
+            }
+            None => None,
+        };
+
+        let mut ch = Self {
+            name: channel_file.name,
+            display_name: channel_file.display_name,
+            description: channel_file.description,
+            url: channel_file.url.trim().to_string(),
+            polling_interval,
+            enabled: false,
+            bundle: None,
+        };
+
+        ch.update_enabled();
+
+        Ok(ch)
+    }
+
+    pub(super) fn from_directory(dir: &str) -> Result<Vec<Self>> {
+        // Find all .yaml files in CHANNELS_DIR
+        let mut dir_entries: Vec<DirEntry> = read_dir(dir)?
+            .filter_map(|dir_entry| dir_entry.ok())
+            .filter(|dir_entry| {
+                dir_entry
+                    .file_name()
+                    .as_os_str()
+                    .as_bytes()
+                    .ends_with(b".yaml")
+            })
+            .collect();
+
+        // And sort them alphabetically, so that 01_stable.yaml takes precedence over
+        // 05_testing.yaml.
+        dir_entries.sort_by_key(|dir_entry| dir_entry.file_name());
+
+        let mut channels: Vec<Self> = Vec::new();
+
+        for dir_entry in dir_entries {
+            let channel = Self::from_file(&dir_entry.path())?;
+
+            if channels.iter().any(|ch| ch.name == channel.name) {
+                bail!("Encountered duplicate channel name \"{}\"", channel.name);
+            }
+
+            channels.push(channel);
+        }
+
+        Ok(channels)
+    }
+
+    fn update_enabled(&mut self) {
+        // Which channels are enabled is decided based on which RAUC certificates are enabled.
+        let cert_file = self.name.clone() + ".cert.pem";
+        let cert_path = Path::new(ENABLE_DIR).join(cert_file);
+
+        self.enabled = cert_path.exists();
+    }
+
+    /// Ask RAUC to determine the version of the bundle on the server
+    pub(super) async fn poll(
+        &mut self,
+        proxy: &InstallerProxy<'_>,
+        slot_status: Option<&SlotStatus>,
+    ) -> Result<()> {
+        self.update_enabled();
+
+        self.bundle = None;
+
+        if self.enabled {
+            let (compatible, version) = proxy.info(&self.url).await?;
+            self.bundle = Some(UpstreamBundle::new(compatible, version, slot_status));
+        }
+
+        Ok(())
+    }
+}
+
+impl UpstreamBundle {
+    fn new(compatible: String, version: String, slot_status: Option<&SlotStatus>) -> Self {
+        let mut ub = Self {
+            compatible,
+            version,
+            newer_than_installed: false,
+        };
+
+        if let Some(slot_status) = slot_status {
+            ub.update_install(slot_status);
+        }
+
+        ub
+    }
+
+    pub(super) fn update_install(&mut self, slot_status: &SlotStatus) {
+        let slot_0_is_older = slot_status
+            .get("rootfs_0")
+            .and_then(|r| r.get("bundle_version"))
+            .and_then(|v| compare_versions(&self.version, v).map(|c| c.is_gt()))
+            .unwrap_or(true);
+
+        let slot_1_is_older = slot_status
+            .get("rootfs_1")
+            .and_then(|r| r.get("bundle_version"))
+            .and_then(|v| compare_versions(&self.version, v).map(|c| c.is_gt()))
+            .unwrap_or(true);
+
+        self.newer_than_installed = slot_0_is_older && slot_1_is_older;
+    }
+}
