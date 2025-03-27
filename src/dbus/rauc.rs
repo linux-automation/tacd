ac2e0c416223552d9a604bab7e33e514e1d518bd
Leonard Göhrs <l.goehrs@pengutronix.de>
2025-03-27 08:59:42 +0100

    tacd: dbus: rauc: re-introspect services
    RAUC is in the process of adding native update polling support.
    Re-introspect the dbus service to add the new APIs.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/dbus/rauc.rs
+++ src/dbus/rauc.rs
@@ -43,6 +43,9 @@ mod installer;
 #[cfg(not(feature = "demo_mode"))]
 use installer::InstallerProxy;
 
+#[cfg(not(feature = "demo_mode"))]
+mod poller;
+
 #[cfg(feature = "demo_mode")]
 mod imports {
     use std::collections::HashMap;
