35aa2440f1a217df6b245fc4f85c01e53b08dcb7
Leonard Göhrs <l.goehrs@pengutronix.de>
2025-04-02 09:42:54 +0200

    dbus: rauc: reload rauc daemon when required
    This uses the existing systemd DBus API integration to trigger a daemon
    restart (there is no support to just reload the config as of now) and
    keep track of the following state changes.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/dbus/rauc.rs
+++ src/dbus/rauc.rs
@@ -20,10 +20,11 @@ use anyhow::Result;
 use async_std::stream::StreamExt;
 use async_std::sync::Arc;
 use futures_util::FutureExt;
-use log::warn;
+use log::{info, warn};
 use serde::{Deserialize, Serialize};
 
 use super::Connection;
+use super::systemd::{Service, ServiceAction};
 use crate::broker::{BrokerBuilder, Topic};
 use crate::watched_tasks::WatchedTasksBuilder;
 
@@ -187,6 +188,7 @@ async fn channel_list_update_task(
     reload: Arc<Topic<bool>>,
     enable_polling: Arc<Topic<bool>>,
     channels: Arc<Topic<Channels>>,
+    rauc_service: Service,
 ) -> Result<()> {
     let (reload_stream, _) = reload.subscribe_unbounded();
     let (mut enable_polling_stream, _) = enable_polling.subscribe_unbounded();
@@ -214,9 +216,43 @@ async fn channel_list_update_task(
             }
         };
 
-        update_system_conf(new_channels.primary(), enable_polling)?;
+        let should_reload = update_system_conf(new_channels.primary(), enable_polling)?;
 
         channels.set(new_channels);
+
+        if should_reload {
+            info!("New RAUC config written. Triggering daemon restart.");
+
+            let (mut status, status_subscription) =
+                rauc_service.status.clone().subscribe_unbounded();
+            rauc_service.action.set(ServiceAction::Restart);
+
+            info!("Waiting for daemon to go down");
+
+            while let Some(ev) = status.next().await {
+                info!("Current status: {} ({})", ev.active_state, ev.sub_state);
+
+                if ev.active_state != "active" {
+                    break;
+                }
+            }
+
+            info!("Waiting for daemon to come up again");
+
+            while let Some(ev) = status.next().await {
+                info!("Current status: {} ({})", ev.active_state, ev.sub_state);
+
+                if ev.active_state == "active" {
+                    break;
+                }
+            }
+
+            info!("Done");
+
+            status_subscription.unsubscribe();
+        } else {
+            info!("Config is up to date. Will not reload.");
+        }
     }
 }
 
@@ -248,6 +284,7 @@ impl Rauc {
         bb: &mut BrokerBuilder,
         wtb: &mut WatchedTasksBuilder,
         _conn: &Arc<Connection>,
+        rauc_service: Service,
     ) -> Result<Self> {
         let inst = Self::setup_topics(bb);
 
@@ -262,6 +299,7 @@ impl Rauc {
                 inst.reload.clone(),
                 inst.enable_polling.clone(),
                 inst.channels.clone(),
+                rauc_service,
             ),
         )?;
 
@@ -273,6 +311,7 @@ impl Rauc {
         bb: &mut BrokerBuilder,
         wtb: &mut WatchedTasksBuilder,
         conn: &Arc<Connection>,
+        rauc_service: Service,
     ) -> Result<Self> {
         let inst = Self::setup_topics(bb);
 
@@ -468,6 +507,7 @@ impl Rauc {
                 inst.reload.clone(),
                 inst.enable_polling.clone(),
                 inst.channels.clone(),
+                rauc_service,
             ),
         )?;
 
