da373069113d3a77dce7debeb10fb772917190a6
Leonard Göhrs <l.goehrs@pengutronix.de>
2025-04-02 14:45:10 +0200

    dbus: systemd: also monitor rauc.service
    This exposes the status of the RAUC daemon to users inside and outside of
    the tacd. As of now neither of them exist, but in the next few commits
    support for re-starting the RAUC deamon to reload its configuration will
    be added.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/dbus/systemd.rs
+++ src/dbus/systemd.rs
@@ -65,6 +65,8 @@ pub struct Systemd {
     pub labgrid: Service,
     #[allow(dead_code)]
     pub iobus: Service,
+    #[allow(dead_code)]
+    pub rauc: Service,
 }
 
 impl ServiceStatus {
@@ -235,6 +237,7 @@ impl Systemd {
         let networkmanager = Service::new(bb, "network-manager");
         let labgrid = Service::new(bb, "labgrid-exporter");
         let iobus = Service::new(bb, "lxa-iobus");
+        let rauc = Service::new(bb, "rauc");
 
         networkmanager
             .connect(wtb, conn.clone(), "NetworkManager.service")
@@ -245,12 +248,14 @@ impl Systemd {
         iobus
             .connect(wtb, conn.clone(), "lxa-iobus.service")
             .await?;
+        rauc.connect(wtb, conn.clone(), "rauc.service").await?;
 
         Ok(Self {
             reboot,
             networkmanager,
             labgrid,
             iobus,
+            rauc,
         })
     }
 }
